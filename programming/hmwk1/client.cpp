b8a69859f4705ba3b2c0c780513205379b056211
shivamchoudhary <sc3973@columbia.edu>
2016-02-05 23:00:45 -0500

    Adding back and forth messages

--- programming/hmwk1/client.cpp
+++ programming/hmwk1/client.cpp
@@ -2,40 +2,63 @@
 #include <exception>
 #include "Common.h"
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 using namespace std;
-struct user_t{
-        string password;
-        string fname;
-}client;
-struct address{
-        int port;
-        string ip;
-}socketname;
-
-
-
-int main(int argc, char *argv[]){
-        if (argc !=4){
-                cerr <<"Wrong Initialization only 3 are required"<<endl;
-                exit(0);
-        }
-        string password   = argv[1];
-        string fname      = argv[2];
-        int port          = stoi(argv[3]);
-        string ip           = "localhost"; 
-        socketname.port  = port;
-        socketname.ip = ip;
-        
-        //Check if the password is less than 16 characters.
-        if (password.length()<16){
-                cerr <<"Password too short use 16 chars";
-        }
-        //Initialize client with password.
-        client.password = password;
-        client.fname = fname;
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
 }
 
-
-
-int 
-
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    while (1){
+            bzero(buffer,256);
+            fgets(buffer,255,stdin);
+            n = write(sockfd,buffer,strlen(buffer));
+            if (n < 0) 
+                 error("ERROR writing to socket");
+            bzero(buffer,256);
+            n = read(sockfd,buffer,255);
+            if (n < 0) 
+                 error("ERROR reading from socket");
+            cout<<"Response"<<buffer;
+    }
+    printf("%s\n",buffer);
+    close(sockfd);
+    return 0;
+}
