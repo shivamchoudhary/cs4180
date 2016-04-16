2dee8d3ec8ade63ed95f4ba9b3cb223f45edd11c
shivamchoudhary <sc3973@columbia.edu>
2016-04-16 13:27:51 -0400

    Adding gitignore

--- programming/hmwk3/ngrams.py
+++ programming/hmwk3/ngrams.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""
+Program to calculate the number of ngrams in the binary.
+"""
 
 __author__ = "Shivam Choudhary"
 __uni__    = "sc3973"
@@ -8,6 +11,8 @@ import os
 import pprint
 import binascii
 import operator
+
+
 class Ngrams(object):
 
     def __init__(self,n,s,in_file,out_file):
@@ -19,33 +24,35 @@ class Ngrams(object):
             in_file:
                 input file name
             out_file:
-                output file name
+                output file name 
         """
         self.n          = n
         self.s          = s
         self.in_file    = in_file
         self.out_file   = out_file
-        self.readfile()
+        self.calcNgrams()
     
-    def readfile(self):
-        ngrams = {}
-        f = open(self.in_file)
-        offset =0
-        size = os.stat(self.in_file).st_size
+    def calcNgrams(self):
+        """ Calculates the Ngrams based on the input given.
+        """
+        ngrams = {} # A hash table to store the calculated ngrams
+        f = open(self.in_file) # Returns the file descriptor for the in_file.
+        offset =0 #Maintain the state of the slide.
+        size = os.stat(self.in_file).st_size # Calculates the size of in_file
         while True:
-            f.seek(offset)
-            chunk = f.read(self.n)
-            offset+=self.s
-            if len(chunk) <self.n:
+            f.seek(offset) # Seek the file to current offset.
+            chunk = f.read(self.n) # Read n bytes from the current seek position.
+            offset+=self.s # update the offset with the slide.
+            if len(chunk) <self.n: # Stop when read returns less than n.
                 break
             else:
+                key = binascii.hexlify(chunk) # hex of chunk used as a key
                 try:
-                    key = binascii.hexlify(chunk)
-                    ngrams[key]+=1
+                    ngrams[key]+=1 # If entry exists in the ngrams hash table.
                 except KeyError:
-                    key = binascii.hexlify(chunk)
-                    ngrams[key] = 1
-        sorted_ngram = sorted(ngrams.items(),key=operator.itemgetter(1),reverse=True)
+                    ngrams[key] = 1 # New key create entry for the key.
+        sorted_ngram = sorted(ngrams.items(),key=operator.itemgetter(1),
+                reverse=True) # Sort the hash table with the vales 
         pprint.pprint(sorted_ngram)
 
 def main():
@@ -63,6 +70,7 @@ def main():
     parser.add_argument("in_file",type=str ,help="the name of the file to analyze")
     parser.add_argument("out_file",type=str, help="the name of the output file")
     args = parser.parse_args()
+
     ngrams = Ngrams(args.n, args.s, args.in_file, args.out_file)
 
 if __name__=="__main__":
