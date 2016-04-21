2e98502059dc2e2e70d48205023fdbb1cfcdded5
root <root@4180-network-security.(none)>
2016-04-20 20:27:17 -0400

    Adding 2c

--- programming/hmwk3/2c.py
+++ programming/hmwk3/2c.py
@@ -2,6 +2,7 @@ import sys
 import os
 import logging
 import random
+import string
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
@@ -13,26 +14,25 @@ class Packet(object):
 		self.sport = sport
 		self.dport = dport
                 self.loopback ="127.0.0.1"
-
+		self.sendTCP()
 	def sendTCP(self):
 		""" Part (a) sending TCP packets
 		"""
-                for i in range(3000,3020):
+                for i in range(3000,3021):
                     send(IP(dst=self.loopback)/TCP(dport=i))
+		self.sendRandom()
 	def sendRandom(self):
 		"""Part(c)  sending random TCP packets
 		"""
-		for i in range(5):
-                    randomstr = ''.join(random.choice(string.letters) 
-                            for x in range(10))
-                    send(IP(dst=self.loopback)/TCP(sport=int(self.sport),
-                        dport=int(self.dport)/Raw(load=randomstr)))
+		for i in range(0,5):
+                    randomstr = ''.join(random.choice(string.letters) for x in range(10))
+                    send(IP(dst=self.loopback)/TCP(sport=self.sport,dport=self.dport)/Raw(load=randomstr))
 
 
 def main():
     try:
-	sport = sys.argv[1]
-	dport = sys.argv[2]	
+	sport = int(sys.argv[1])
+	dport = int(sys.argv[2])
     except IndexError:
 	print "Error: Run as python <c1.py> <source_port> <dst_port> "	
 	exit(0)
