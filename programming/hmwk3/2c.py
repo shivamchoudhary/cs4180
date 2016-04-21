01bb0b610f417d171dd839dcb600177a47ccb569
root <root@4180-network-security.(none)>
2016-04-20 20:09:43 -0400

    Adding 2c

--- programming/hmwk3/2c.py
+++ programming/hmwk3/2c.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import logging
+import random
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import *
 
@@ -18,7 +19,12 @@ class Packet(object):
 		"""
                 for i in range(3000,3020):
                     send(IP(dst=self.loopback)/TCP(dport=i))
-
+	def sendRandom(self):
+		"""Part(c)  sending random TCP packets
+		"""
+		for i in range(5):
+			randomstr = "".join(random.choice(string.letters) for i in range(10))
+			send(IP(dst=self.loopback)/TCP(sport=int(self.sport),dport=int(self.dport))/Raw(load=randomstr)	
 
 
 
@@ -29,5 +35,6 @@ def main():
 	except IndexError:
 		print "Error: Run as python <c1.py> <source_port> <dst_port> "	
 		exit(0)
+	p = Packet(sport,dport)
 if __name__=="__main__":
 	main()
